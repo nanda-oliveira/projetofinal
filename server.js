e77230fb46b747080a9d0ab102e6f2713e57506e
nanda-oliveira <fernandaoliveiraufpe@gmail.com>
2020-12-10 23:38:02 -0300

    change PORT to 5050

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const app = require("./src/app")
 const dotenv = require('dotenv')
 dotenv.config();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5050
 
 
-app.listen(PORT,function(){
-    console.log(`APP listening at http://localhost:${PORT}`)
-})
+//app.listen(PORT,function(){
+ //   console.log(`APP listening at http://localhost:${PORT}`)
+//})
 
 //module.exports =  app 
+
+app.listen(process.env.PORT, '0.0.0.0', function(){
+  console.log(`APP listening at http://localhost:${PORT}`)
+})
